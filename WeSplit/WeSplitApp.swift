a13667c4f799ce450bf40e44ca14b9450d4aa40e
Tien Dao <tiendao5240@gmail.com>
2022-10-17 15:47:58 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Tien Dao on 2022-10-17.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
