e5c3fc50835907793dd6f257fddd2da2c2a69a5e
Tien Dao <tiendao5240@gmail.com>
2022-10-18 16:02:15 -0400

    Project1: Done

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -18,9 +18,21 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
     
     let tipPercentages = [10, 15, 20, 25, 0]
 
+    var totalPerPerson: Double {
+        //calculate the total per person here
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+    }
     
     var body: some View {
         NavigationView {
@@ -37,6 +49,7 @@ struct ContentView: View {
                         TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                             .keyboardType(.decimalPad)
                         //                        .keyboardType(.numberPad)
+                            .focused($amountIsFocused)
                         Picker("Number of people", selection: $numberOfPeople) {
                             ForEach(2..<100) {
                                 Text("\($0) people")
@@ -50,20 +63,44 @@ struct ContentView: View {
                     }
                 }
                 
+                Section {
+                    
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave")
+                } //That uses multiple trailing closures to specify both the section body (the first closure) and the second header (the second closure).
+                
+                
                 if #available(iOS 16, *) {
                     // Run code in iOS 16 or later.
                     Section {
-                        Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    } header: {
+                        Text("Amount per person")
                     }
                 } else {
                     // Fall back to earlier iOS APIs.
                     Section {
-                        Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                        Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
                     }
                 }
                 
             }
             .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
         
     }
