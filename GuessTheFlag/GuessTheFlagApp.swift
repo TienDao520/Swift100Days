5466b75e54048ff9939c88f12e42eb31c26072a5
Tien Dao <tiendao5240@gmail.com>
2022-10-18 16:57:30 -0400

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Tien Dao on 2022-10-18.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
