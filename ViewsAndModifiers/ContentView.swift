692624b439af2ef00e0213d39a03a39dbc917b11
Tien Dao <tiendao5240@gmail.com>
2022-10-19 14:57:48 -0400

    Conditional modifiers

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,20 +7,34 @@
 
 import SwiftUI
 
-///https://www.hackingwithswift.com/books/ios-swiftui/why-does-swiftui-use-some-view-for-its-view-type
-///First, using some View is important for performance: SwiftUI needs to be able to look at the views we are showing and understand how they change, so it can correctly update the user interface. If SwiftUI didn’t have this extra information, it would be really slow for SwiftUI to figure out exactly what changed – it would pretty much need to ditch everything and start again after every small change.
+///https://www.hackingwithswift.com/books/ios-swiftui/conditional-modifiers
+
 struct ContentView: View {
-    /// if you create a VStack with two text views inside, SwiftUI silently creates a TupleView to contain those two views – a special type of view that holds exactly two views inside it. So, the VStack fills the “what kind of view is this?” with the answer “it’s a TupleView containing two text views.”
-    
-//    And what if you have three text views inside the VStack? Then it’s a TupleView containing three views.
-    //Swift silently applies a special attribute to the body property called @ViewBuilder. This has the effect of silently wrapping multiple views in one of those TupleView containers, so that even though it looks like we’re sending back multiple views they get combined into one TupleView.
+    @State private var useRedText = false
+
     var body: some View {
-        Button("Hello, world!") {
-            print(type(of: self.body))
+        Button("Hello World") {
+            // flip the Boolean between true and false
+            useRedText.toggle()
         }
-        .frame(width: 200, height: 200)
-        .background(.red)
+        .foregroundColor(useRedText ? .red : .blue)
     }
+    /**Sometimes using if statements are unavoidable, but where possible prefer to use the ternary operator instead.
+     */
+//    var body: some View {
+//        if useRedText {
+//            Button("Hello World") {
+//                useRedText.toggle()
+//            }
+//            .foregroundColor(.red)
+//        } else {
+//            Button("Hello World") {
+//                useRedText.toggle()
+//            }
+//            .foregroundColor(.blue)
+//        }
+//    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
