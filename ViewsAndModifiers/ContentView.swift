b60be776eed7f8181c624b686330958a166483ed
Tien Dao <tiendao5240@gmail.com>
2022-10-19 14:38:55 -0400

    Modifier order

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,11 +7,25 @@
 
 import SwiftUI
 
+///https://www.hackingwithswift.com/books/ios-swiftui/why-modifier-order-matters
+///Whenever we apply a modifier to a SwiftUI view, we actually create a new view with that change applied – we don’t just modify the existing view in place. If you think about it, this behavior makes sense: our views only hold the exact properties we give them, so if we set the background color or font size there is no place to store that data.
 struct ContentView: View {
     var body: some View {
+        Button("Hello, world!") {
+            print(type(of: self.body))
+        }
+        .frame(width: 200, height: 200)
+        .background(.red)
+//        .frame(width: 200, height: 200)
         Text("Hello, world!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding()
             .background(.red)
+            .padding()
+            .background(.blue)
+            .padding()
+            .background(.green)
+            .padding()
+            .background(.yellow)
     }
 }
 
