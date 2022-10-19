851efa53877b5c9dec50996e4d010dc8f6332491
Tien Dao <tiendao5240@gmail.com>
2022-10-19 14:53:34 -0400

    some View for view types

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,25 +7,19 @@
 
 import SwiftUI
 
-///https://www.hackingwithswift.com/books/ios-swiftui/why-modifier-order-matters
-///Whenever we apply a modifier to a SwiftUI view, we actually create a new view with that change applied – we don’t just modify the existing view in place. If you think about it, this behavior makes sense: our views only hold the exact properties we give them, so if we set the background color or font size there is no place to store that data.
+///https://www.hackingwithswift.com/books/ios-swiftui/why-does-swiftui-use-some-view-for-its-view-type
+///First, using some View is important for performance: SwiftUI needs to be able to look at the views we are showing and understand how they change, so it can correctly update the user interface. If SwiftUI didn’t have this extra information, it would be really slow for SwiftUI to figure out exactly what changed – it would pretty much need to ditch everything and start again after every small change.
 struct ContentView: View {
+    /// if you create a VStack with two text views inside, SwiftUI silently creates a TupleView to contain those two views – a special type of view that holds exactly two views inside it. So, the VStack fills the “what kind of view is this?” with the answer “it’s a TupleView containing two text views.”
+    
+//    And what if you have three text views inside the VStack? Then it’s a TupleView containing three views.
+    //Swift silently applies a special attribute to the body property called @ViewBuilder. This has the effect of silently wrapping multiple views in one of those TupleView containers, so that even though it looks like we’re sending back multiple views they get combined into one TupleView.
     var body: some View {
         Button("Hello, world!") {
             print(type(of: self.body))
         }
         .frame(width: 200, height: 200)
         .background(.red)
-//        .frame(width: 200, height: 200)
-        Text("Hello, world!")
-            .padding()
-            .background(.red)
-            .padding()
-            .background(.blue)
-            .padding()
-            .background(.green)
-            .padding()
-            .background(.yellow)
     }
 }
 
