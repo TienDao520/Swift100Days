e08229fdf70b6003dc9160b0dd8e8594764bb57d
Tien Dao <tiendao5240@gmail.com>
2022-10-20 11:00:05 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Tien Dao on 2022-10-20.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
