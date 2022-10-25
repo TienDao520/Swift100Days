a6a985ece6b187939215c0a0f563ffcc5518b905
Tien Dao <tiendao5240@gmail.com>
2022-10-25 18:32:30 -0400

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Tien Dao on 2022-10-25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
